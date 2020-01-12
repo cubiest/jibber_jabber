0c8ad0ef5d16dadce3ab786d22ab56b572b4365c
MJacred <loesch.benny92@gmx.de>
2020-01-12 15:34:21 +0100

    use len() instead of equal ""

--- jibber_jabber_unix.go
+++ jibber_jabber_unix.go
@@ -20,20 +20,20 @@ func getLangFromEnv() (locale string) {
 	return
 }
 
-func getUnixLocale() (unix_locale string, err error) {
-	unix_locale = getLangFromEnv()
-	if unix_locale == "" {
+func getUnixLocale() (locale string, err error) {
+	locale = getLangFromEnv()
+	if len(locale) <= 0 {
 		err = errors.New(COULD_NOT_DETECT_PACKAGE_ERROR_MESSAGE)
 	}
 	return
 }
 
 func DetectIETF() (locale string, err error) {
-	unix_locale, err := getUnixLocale()
+	locale, err := getUnixLocale()
 	if err == nil {
-		language, territory := splitLocale(unix_locale)
+		language, territory := splitLocale(locale)
 		locale = language
-		if territory != "" {
+		if len(territory) > 0 {
 			locale = strings.Join([]string{language, territory}, "-")
 		}
 	}
@@ -41,17 +41,17 @@ func DetectIETF() (locale string, err error) {
 }
 
 func DetectLanguage() (language string, err error) {
-	unix_locale, err := getUnixLocale()
+	locale, err := getUnixLocale()
 	if err == nil {
-		language, _ = splitLocale(unix_locale)
+		language, _ = splitLocale(locale)
 	}
 	return
 }
 
 func DetectTerritory() (territory string, err error) {
-	unix_locale, err := getUnixLocale()
+	locale, err := getUnixLocale()
 	if err == nil {
-		_, territory = splitLocale(unix_locale)
+		_, territory = splitLocale(locale)
 	}
 	return
 }
